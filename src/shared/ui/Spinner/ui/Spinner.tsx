f42d6e01baac8d7121d68cca5ea0e6188d89fddc
Volodymyr_Pavlov <volodymyr.p@thewhitelabelagency.com>
2024-06-22 01:16:27 +0300

    Added NotFoundPage, PageLoader and Spinner.

---
+++ src/shared/ui/Spinner/ui/Spinner.tsx
@@ -0,0 +1,12 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import './Spinner.scss';
+
+interface SpinnerProps {
+    className?: string;
+}
+
+export const Spinner = ({className}: SpinnerProps) => {
+    return (
+        <div className={classNames('lds-dual-ring', {}, [className])} />
+    );
+};
